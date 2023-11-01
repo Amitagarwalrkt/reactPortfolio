395742852cc6269a2271ae5b1e5ace85c5634b03
Amitagarwalrkt <107742055+Amitagarwalrkt@users.noreply.github.com>
2023-11-01 19:42:42 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "AANCHAL";
+const meta = {
+    title: "Aanchal Bisht",
+    description: "I’m Aanchal Bisht data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Aanchal bisht",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I am a student in the College of Computer Sciences at the Graphic Era Hill University, pursuing an BCA. I am majoring in Computer Science, with intent to double major in Mathematics with Statistics and Data Science concentration.",
+    your_img: "./src/assets/image/home.jpeg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a student in the College of Computer Sciences at the Graphic Era Hill University, pursuing an BCA. I am majoring in Computer Science, with intent to double major in Mathematics with Statistics and Data Science concentration. I'm involved as a Software Developer for BUILD UMass, where I use my software engineering skills for pro-bono app/web development and tech consulting for the Amherst community. I also serve as a Designer for the HackUMass Organizing Team, responsible for graphics design and production for the annual hackathon. I am interested in exploring the fields of Software Engineering, Data Science, and Machine Learning, and I am open to opportunities for the same.",
+};
+const education= [{
+        jobtitle: "Bachlors",
+        where: "Graphic Era Hill Univesity",
+        date: "2024",
+    },
+    {
+        jobtitle: "Intermediate",
+        where: "S.B.V.M. inter college",
+        date: "2021",
+    },
+    {
+        jobtitle: "High School",
+        where: "S.B.V.M. inter college",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "C++",
+        value: 60,
+    },
+    {
+        name: "Java",
+        value: 70,
+    },
+    {
+        name: "MS Excel",
+        value: 60,
+    },
+    {
+        name: "SQl",
+        value: 65,
+    },
+];
+
+const services = [{
+        title: "FRONT-END",
+        description: "Expertise in HTML, CSS, and JavaScript: Showcase your proficiency in building responsive and visually appealing websites using these core front-end technologies.",
+    },
+    {
+        title: "Back-End",
+        description: "Proficiency in server-side programming languages: Highlight your skills in languages like Python, Java, or Node.js, and explain how you have used them to build robust back-end systems.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "User interface design for mobile: Showcase your ability to create visually appealing and intuitive mobile app interfaces that provide a smooth user experience.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "aanchalbisht042gmail.com",
+    YOUR_FONE: "+91871031907",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_n52ux78",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "O4ZGGCuXO3C3-LnnC",
+};
+
+const socialprofils = {
+    github: "https://github.com/Aanchalbisht",
+    facebook: "https://www.facebook.com/profile.php?id=100072142864039",
+    linkedin: "https://www.linkedin.com/in/aanchal-bisht-781ba3290/",
+    twitter: "https://twitter.com",
+    whatsapp:"",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    education,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
